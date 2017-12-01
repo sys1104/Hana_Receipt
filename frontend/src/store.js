348d24513f372121bb4def3bc85ddf9c7b09aec1
JeonHanShin <hanshin234@naver.com>
2017-12-01 10:27:50 +0900

    초안 수정(2)

---
+++ frontend/src/store.js
@@ -0,0 +1,21 @@
+export const store = {
+    stories: [{
+            plot: 'Hello........',
+            writer: 'mhso',
+            upvotes: 20,
+            voted: false
+        },
+        {
+            plot: 'aaa',
+            writer: 'bbb',
+            upvotes: 50,
+            voted: false
+        },
+        {
+            plot: 'ccc',
+            writer: 'eee',
+            upvotes: 20,
+            voted: false
+        }
+    ]
+}
