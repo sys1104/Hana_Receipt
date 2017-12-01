86cbcd66cfa49172ba21a05d63425b58720a7b0c
JeonHanShin <hanshin234@naver.com>
2017-12-01 09:57:43 +0900

    초안 변경

---
+++ frontend/src/components/Famous.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="famous">
+        <h2>Trending stories<strong>({{famous.length}})</strong></h2>
+        <ul class="list-group">
+            <li v-for="story in famous" class="list-group-item">
+                {{story.writer}} said "{{story.plot}}". Story upvotes {{story.upvotes}}.
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import {store} from '../store.js'
+export default {
+    computed:{
+        famous:function(){
+            return this.stories.filter(function(item){
+                return item.upvotes>25;
+            })
+        }
+    },
+    data(){
+        return {
+            stories: store.stories
+        }
+    }
+    //props: ['stories']
+}
+</script>
+
---
+++ frontend/src/components/Stories.vue
@@ -0,0 +1,20 @@
+<template id="stories">
+    <ul class="list-group">
+        <li class="list-group-item" v-for="story in store.stories">
+            {{story.writer}} said "{{story.plot}}"
+            Story upvotes{{story.upvotes}}
+        </li>
+    </ul>    
+</template>
+
+<script>
+    import {store} from '../store.js' //컴포넌트 임포트 할 때는 중괄호 no, js 파일 임포트에는 중괄호 yes
+    export default {
+        //props: ['stories']
+        data(){
+            return {
+                store
+            }
+        }
+    }
+</script>
