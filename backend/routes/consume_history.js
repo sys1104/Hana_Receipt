40e8df603f7eb55cc145c5a3e222d1a26aafaeea
JeonHanShin <hanshin234@naver.com>
2017-12-05 16:29:58 +0900

    낭비 내역 구현 중

--- backend/routes/consume_history.js
+++ backend/routes/consume_history.js
@@ -304,6 +304,89 @@ var consumeList = function(req, res) {
     res.end();
   }
 };
+
+
+
+
+
+
+
+// ========================= server-side 1주일간 낭비되는 금액 카테고리별 출력 function =================== //
+var wasted_category_list = function(req, res, callback) {
+  console.log('********** server-side 목표대비 사용금액 분석 function 호출 **********');
+  var database = req.app.get('database');
+  var u_num = ''; //vue에서 받아와야 함!!!
+  var paramCatenum = req.body.cate_num;
+  var start_date = ''; //front단에서 오늘 기준으로 주일의 시작
+  var end_date = ''; //front단에서 오늘 기준으로 주일의 마지막날로 정의한 것을 받아옴
+  if (database) {
+    var data = {};
+    wasted_used(database, u_num, start_date, end_date, function(err, rows) {
+      if (err) {
+        console.error('********** cate_used 에러 발생 **********' + err.stack);
+        res.writeHead(200, {
+          "Content-Type": 'text/html;charset=utf8'
+        });
+        res.write('<h2>cate_used 에러 발생</h2>');
+        res.write('<p>' + err.stack + '</p>');
+        res.end();
+        // 에러 처리
+      }
+      if (row) {
+        data.wasted_used = rows;
+      } else {
+        console.log("********** 사용내역 없음 **********");
+      }
+    });
+    res.json(data);
+    console.log(data);
+    res.end();
+  } else {
+    //DB접속에 실패 했을 경우
+    res.writeHead(200, {
+      "Content-Type": 'text/html;charset=utf8'
+    });
+    res.write('<h1>데이터베이스 연결 실패</h1>');
+    res.write('<div><p>DB에 연결 하지 못했습니다</p></div>');
+    res.write("<br/><a href='/public/login2.html'>다시 로그인</a>");
+    res.end();
+  }
+};
+
+// ========================= server-side 카테고리별 사용금액 분석 function =================== //
+var wasted_used = function(database, u_num, start_date, end_date, callback) {
+  console.log('********** server-side wasted_used 호출 **********');
+  database.pool.getConnection(function(err, conn) { //DB 접속
+    if (err) {
+      if (conn) {
+        conn.release();
+      }
+      callback(err, null);
+      return;
+    }
+    console.log('데이터베이스 연결 스레드 아이디 : ' + conn.threadId);
+    var exec = conn.query('select cate_num, sum(price) from consume_history where wasted = 1 and time >= ? and time <= ?',
+      u_num, start_date, end_date,
+      function(err, rows) {
+        //select의 결과물은 배열로 들어온다. rows 변수...
+        if (rows.length > 0) {
+          console.log('********** 카테고리별 낭비금액 rows로 반환 **********');
+          callback(null, rows);
+        } else {
+          console.log('********** 사용내역이 없습니다.**********');
+          callback(null, null);
+        }
+      });
+    conn.on('error', function(err) {
+      console.log('********** 데이터베이스 연결 시 에러 발생함 **********');
+      console.dir(err);
+      callback(err, null);
+    });
+  });
+};
+
+
 module.exports.requestHistory = requestHistory;
 module.exports.updateHistory = updateHistory;
 module.exports.consumeList = consumeList;
+module.exports.wasted_category_list = wasted_category_list;
--- frontend/src/components/Wasted.vue
+++ frontend/src/components/Wasted.vue
@@ -4,32 +4,22 @@
         <br><br>
         <hr>
         <div class="col-md-12">
-                      
+
             <div class="col-md-offset-3 col-md-6 box-container">
-                <h3>낭비되고 있는 금액</h3>
+                <h3>지난 주 낭비된 금액</h3>
                 <table class="table table-striped table-hover table-responsive">
                   <tr class="table-header">
                       <td>카테고리</td>
                       <td>금액</td>
-                  </tr>        
-                  <tr>
-                   <td>음료</td>
-                   <td>30000</td>
-                  </tr>      
-                  <tr>
-                   <td>교통</td>
-                   <td>100000</td>
-                  </tr>      
-                  <tr>
-                   <td>식비</td>
-                   <td>80000</td>
                   </tr>
+                  <tr class="table-body" v-for="(result,index) in results">
+
 
                 </table>
                 <hr>
                 <h3 class="text-right">총액 : 210000원</h3>
             </div>
-            
+
         </div>
     </div>
 </template>
