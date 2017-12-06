01cc16428aa8b98f510e60064aade62ee161a7be
yssong <thddudtn12@naver.com>
2017-12-06 17:49:04 +0900

    메인충돌해결, 목표설정 작업중

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from 'vue-router'
-<<<<<<< HEAD
-import VuePaginator from 'vuejs-paginator'
-=======
 
-//세션
->>>>>>> 50a8734bc9b6571e02d11415c6ba1ffe32abc657
 import VueSession from 'vue-session'
 //세션
 import VueResource from 'vue-resource'
@@ -30,101 +25,82 @@ import ModifyUser from './components/ModifyUser.vue'
 import GoalManagement from './components/GoalManagement.vue'
 
 Vue.use(VueRouter)
-// Vue.use(VueResource)
-// Vue.use(VuePaginator)
+    // Vue.use(VueResource)
+    // Vue.use(VuePaginator)
 Vue.use(VueSession)
 Vue.use(axios)
-//사용할 라우터 객체등록
+    //사용할 라우터 객체등록
 const routes = [{
-    name: 'home',
-    path: '/',
-    component: Main
-  },
-  {
-    name: 'regist',
-    path: '/register',
-    component: Register
-  },
-  {
-    name: 'stories',
-    path: '/stories',
-    component: Stories
-  },
-  {
-    name: 'login',
-    path: '/login',
-    component: Login
-  },
-  {
-    name: 'first_section',
-    path: 'first_section',
-    component: FirstSection
-  },
-  {
-    name: 'store_test',
-    path: '/store_test',
-    component: Storetest
-  },
-  {
-    name: 'modify_user',
-    path: '/modify_user',
-    component: ModifyUser
-  },
-  {
-    name: 'goal_management',
-    path: '/goal_management',
-    component: GoalManagement
-  },
+            name: 'home',
+            path: '/',
+            component: Main
+        },
+        {
+            name: 'regist',
+            path: '/register',
+            component: Register
+        },
+        {
+            name: 'stories',
+            path: '/stories',
+            component: Stories
+        },
+        {
+            name: 'login',
+            path: '/login',
+            component: Login
+        },
+        {
+            name: 'first_section',
+            path: 'first_section',
+            component: FirstSection
+        },
+        {
+            name: 'store_test',
+            path: '/store_test',
+            component: Storetest
+        },
+        {
+            name: 'modify_user',
+            path: '/modify_user',
+            component: ModifyUser
+        },
+        {
+            name: 'goal_management',
+            path: '/goal_management',
+            component: GoalManagement
+        },
 
-  {
-    path: './stories',
-    component: StoriesPage,
-    children: [{
-        path: '',
-        name: 'stories.all',
-        component: StoriesAll
-      },
-      {
-        path: 'famous',
-        name: 'stories.famous',
-        component: StoriesFamous
-      }
+        {
+            path: './stories',
+            component: StoriesPage,
+            children: [{
+                    path: '',
+                    name: 'stories.all',
+                    component: StoriesAll
+                },
+                {
+                    path: 'famous',
+                    name: 'stories.famous',
+                    component: StoriesFamous
+                }
+            ]
+        }
     ]
-  }
-]
-//라우터 객체 생성
+    //라우터 객체 생성
 const router = new VueRouter({
-<<<<<<< HEAD
-  mode: 'history',
-  base: '/',
-  routes
-});
-Vue.config.productionTip = false;
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router, //어플리케이션 전체에서 라우터 인식 시키기
-  template: '<App/>',
-  components: {
-    App
-  }
-});
-=======
+
     mode: 'history',
     base: '/',
     routes
 });
-
-
 Vue.config.productionTip = false;
-
-
-
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
     router, //어플리케이션 전체에서 라우터 인식 시키기
     template: '<App/>',
-    components: { App }
-})
->>>>>>> 50a8734bc9b6571e02d11415c6ba1ffe32abc657
+    components: {
+        App
+    }
+});
