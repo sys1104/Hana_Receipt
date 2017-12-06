23d891541b74762f43a0e0adc27ce9f7a31929ee
kwonsohyeon <thgus1023@hanmail.net>
2017-12-06 15:16:14 +0900

    그래프 추가

---
+++ frontend/src/components/MyCategoryChart.vue
@@ -0,0 +1,121 @@
+
+<template>
+    <div id='myCategoryChart'></div>
+</template>
+
+<script>
+// 카테고리별 목표금액 그래프
+    export default {
+        data(){
+            return{
+                myCategoryConfig: {
+                    type: "hbar",
+                    backgroundColor: "#FCFCFC",
+                    tooltip: { visible: false },
+                    scaleX: {
+                        lineColor: "transparent",
+                        tick: {
+                            visible: false
+                        },
+                        labels: ["음료", "식비", "교통", "통신"],
+                        item: {
+                            fontColor: "#000000",
+                            fontSize: 16
+                        }
+                    },
+                    scaleY: {
+                        visible: false,
+                        lineColor: "transparent",
+                        guide: {
+                            visible: false
+                        },
+                        tick: {
+                            visible: false
+                        }
+                    },
+                    plotarea: {
+                        marginLeft: "80",
+                        marginTop: "30",
+                        marginBottom: "0"
+                    },
+                    plot: {
+                        stacked: true,
+                        barsSpaceLeft: "20px",
+                        barsSpaceRight: "20px",
+                        valueBox: {
+                            visible: true,
+                            text: "%v0%",
+                            fontColor: "#2A2B3A",
+                            fontSize: 14
+                        },
+                        tooltip: {
+                            borderWidth: 0,
+                            borderRadius: 2
+                        },
+                        animation: {
+                            effect: 3,
+                            sequence: 3,
+                            method: 3
+                        }
+                    },
+                    series: [{
+                            values: [3, 2, 6, 7],
+                            borderRadius: "50px 0px 0px 50px",
+                            backgroundColor: "#E71D36",
+                            rules: [{
+                                    rule: "%i === 0",
+                                    backgroundColor: "#E71D36"
+                                },
+                                {
+                                    rule: "%i === 1",
+                                    backgroundColor: "#2EC4B6"
+                                },
+                                {
+                                    rule: "%i === 2",
+                                    backgroundColor: "#FF9F1C"
+                                },
+                                {
+                                    rule: "%i === 3",
+                                    backgroundColor: "#20A665"
+                                }
+
+                            ]
+                        },
+                        {
+                            values: [7, 8, 4, 3],
+                            borderRadius: "0px 50px 50px 0px",
+                            backgroundColor: "#E71D36",
+                            //alpha : 0.8,
+                            rules: [{
+                                    rule: "%i === 0",
+                                    backgroundColor: "#e85d6f"
+                                },
+                                {
+                                    rule: "%i === 1",
+                                    backgroundColor: "#90eae2"
+                                },
+                                {
+                                    rule: "%i === 2",
+                                    backgroundColor: "#f7be70"
+                                },
+                                {
+                                    rule: "%i === 3",
+                                    backgroundColor: "#5FAD47"
+                                }
+                            ]
+                        }
+                    ]
+                }
+            }
+        },
+
+        mounted(){
+            zingchart.render({
+                id: 'myCategoryChart',
+                data: this.myCategoryConfig,
+                height: '70%',
+                width: '99%'
+            });
+        }
+    }
+</script>
---
+++ frontend/src/components/PercentageChart.vue
@@ -0,0 +1,180 @@
+
+<template>
+    <div id='percentageChart'></div>
+</template>
+
+<script>
+// 나와 이용자 평균 percentage
+    export default {
+        data(){
+            return{
+                percentageConfig: {
+    "layout":"h",
+     "globals":{
+        "font-family":"Roboto"
+    },
+    "graphset":[
+        // 첫번째 파이
+        {
+            "type":"pie",
+            "background-color":"none",
+            "legend":{
+                "background-color":"none",
+                "border-width":0,
+                "shadow":false,
+                "layout":"float",
+                "margin":"auto auto 8% auto",
+                "marker":{
+                    "border-radius":3,
+                    "border-width":0
+                },
+                "item":{
+                    "color":"%backgroundcolor"
+                }
+            },
+            // "title":{
+            //     // "text":"카테고리별 나의 소비량",
+            //     "background-color":"none",
+            //     "font-size":16,
+            //     "color":"",
+            //     "x":-20,
+            //     "y":80
+            // },
+            "plotarea":{
+                "background-color":"none",
+                "border-color":"none",
+                "border-width":1,
+                "border-radius":3,
+                "margin":"15% 5%"
+            },
+            "labels":[
+                {
+                    "x":"45%",
+                    "y":"47%",
+                    "width":"10%",
+                    "text":"나의 평균 소비",
+                    "font-size":18
+                }    
+            ],
+            "plot":{
+                "size":100,
+                "slice":70,
+                "margin-right":100,
+                "border-width":0,
+                "shadow":0,
+                "value-box":{
+                    "visible":false
+                },
+                "tooltip":{
+                    "text":"%v원",
+                    "shadow":false,
+                    "border-radius":3
+                }
+            },
+            "series":[
+                {
+                    "values":[136],
+                    "text":"식비",
+                    "background-color":"#E85D6F"
+                },
+                {
+                    "values":[118],
+                    "text":"교통",
+                    "background-color":"#F7BE70"
+                },
+                {
+                    "values":[108],
+                    "text":"통신",
+                    "background-color":"#90EAE2"
+                }
+            ]
+        },
+        // 두번째 파이
+        {
+            "type":"pie",
+            "background-color":"none",
+            "legend":{
+                "background-color":"none",
+                "border-width":0,
+                "shadow":false,
+                "layout":"float",
+                "margin":"auto auto 8% auto",
+                "marker":{
+                    "border-radius":3,
+                    "border-width":0
+                },
+                "item":{
+                    "color":"%backgroundcolor"
+                }
+            },
+            "title":{
+                // "text":"카테고리별 평균 소비량",
+                "background-color":"none",
+                "color":"#626262",
+                "font-size":16,
+                "x":-52,
+                "y":80
+            },
+            "plotarea":{
+                "background-color":"none",
+                "border-color":"none",
+                "border-width":1,
+                "border-radius":3,
+                "margin":"10% 5%"
+            },
+            "labels":[
+                {
+                    "x":"45%",
+                    "y":"47%",
+                    "width":"10%",
+                    "text":"이용자 평균 소비",
+                    "font-size":18
+                }    
+            ],
+            "plot":{
+                "size":100,
+                "slice":70,
+                "border-width":0,
+                "shadow":0,
+                "value-box":{
+                    "visible":false
+                },
+                // 마우스 올릴 때
+                 "tooltip":{
+                    "text":"%v원",
+                    "shadow":false,
+                    "border-radius":3
+                }
+            },
+            "series":[
+                {
+                    "values":[2.33],
+                    "text":"식비",
+                    "background-color":"#E85D6F"
+                },
+                {
+                    "values":[2.02],
+                    "text":"교통",
+                    "background-color":"#F7BE70"
+                },
+                {
+                    "values":[1.85],
+                    "text":"통신",
+                    "background-color":"#90EAE2"
+                }
+            ]
+        }
+    ]}
+            }
+        },
+
+        mounted(){
+            zingchart.render({ 
+            id : 'percentageChart', 
+            data : this.percentageConfig, 
+            height: '75%', 
+            width: '99%'
+        });
+    }
+}
+</script>
