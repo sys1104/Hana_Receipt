749dbdfb9a456c0a1b8fda4b049f18e7be168185
강두영 <kangdy5491@naver.com>
2017-12-07 16:03:15 +0900

    목표저장부분 개발중

--- frontend/src/components/GoalManagement.vue
+++ frontend/src/components/GoalManagement.vue
@@ -1,24 +1,69 @@
 <template>
 <div id="insertGoal">
   <navi></navi>
-  <div class="container">
+  <div class="container" style="display:inline-block">
     <wasted></wasted>
     <h2>목표 저장하기</h2>
-    <div class="form-group">
-      <select v-model="cate_num" class="form-control" name="cate_num">
-        <option value="1">생활/쇼핑</option>
-        <option value="2">교통</option>
-        <option value="3">식비</option>
-        <option value="4">패션/미용</option>
-        <option value="5">주거/통신</option>
-        <option value="6">미분류</option>
-       </select>
-    </div>
-    <div class="form-group">
-      <input type="text" placeholder="목표기간" v-model="g_time" class="form-control" name="g_time">
-    </div>
-    <div class="form-group">
-      <input type="text" placeholder="목표금액" v-model="g_price" class="form-control" name="g_price">
+    <!-- class="row" 추가 -->
+    <div class="row">
+      <div class="form-group" style="width:20%;">
+        <label class="form-control">목표기간</label>
+        <!-- <input type="hidden" v-model="cate_num" class="form-control" value="1" name="cate_num"> -->
+      </div>
+      <div class="form-group" style="width:30%">
+        <input type="text" placeholder="시작날짜" v-model="g_time" class="form-control" name="g_time">
+      </div>
+      <div class="form-group" style="width:30%">
+        <input type="text" placeholder="마지막날짜" v-model="g_endtime" class="form-control" name="g_endtime">
+      </div>
+
+      <div class="form-group" style="width:30%">
+        <label class="form-control">생활/쇼핑</label>
+        <input type="hidden" v-model="cate_num.cate_num1" class="form-control" value="1" name="cate_num1">
+      </div>
+      <div class="form-group" style="width:50%">
+        <input type="text" placeholder="목표금액" v-model="g_price.g_price1" class="form-control" name="g_price1">
+      </div>
+
+      <div class="form-group" style="width:30%">
+        <label class="form-control">교통</label>
+        <input type="hidden" v-model="cate_num.cate_num2" class="form-control" value="2" name="cate_num2">
+      </div>
+      <div class="form-group" style="width:50%">
+        <input type="text" placeholder="목표금액" v-model="g_price.g_price2" class="form-control" name="g_price2">
+      </div>
+
+      <div class="form-group" style="width:30%">
+        <label class="form-control">식비</label>
+        <input type="hidden" v-model="cate_num.cate_num3" class="form-control" value="3" name="cate_num3">
+      </div>
+      <div class="form-group" style="width:50%">
+        <input type="text" placeholder="목표금액" v-model="g_price.g_price3" class="form-control" name="g_price3">
+      </div>
+
+      <div class="form-group" style="width:30%">
+        <label class="form-control">패션/미용</label>
+        <input type="hidden" v-model="cate_num.cate_num4" class="form-control" value="4" name="cate_num4">
+      </div>
+      <div class="form-group" style="width:50%">
+        <input type="text" placeholder="목표금액" v-model="g_price.g_price4" class="form-control" name="g_price4">
+      </div>
+
+      <div class="form-group" style="width:30%">
+        <label class="form-control">주거/통신</label>
+        <input type="hidden" v-model="cate_num.cate_num5" class="form-control" value="5" name="cate_num5">
+      </div>
+      <div class="form-group" style="width:50%">
+        <input type="text" placeholder="목표금액" v-model="g_price.g_price5" class="form-control" name="g_price5">
+      </div>
+
+      <div class="form-group" style="width:30%">
+        <label class="form-control">미분류</label>
+        <input type="hidden" v-model="cate_num.cate_num6" class="form-control" value="6" name="cate_num6">
+      </div>
+      <div class="form-group" style="width:50%">
+        <input type="text" placeholder="목표금액" v-model="g_price.g_price6" class="form-control" name="g_price6">
+      </div>
     </div>
     <button @click.prevent="goalStore" class="btn btn-success">저장</button>
   </div>
@@ -36,12 +81,27 @@ import Navi from './Navi.vue'
 export default {
   data: function() {
     return {
-      u_num:'',
       results:'',
-      cate_num: '',
+      u_num:'',
+      g_num:'',
+      cate_num: {
+        cate_num1:1,
+        cate_num2:2,
+        cate_num3:3,
+        cate_num4:4,
+        cate_num5:5,
+        cate_num6:6,
+      },
       g_time: '',
-      g_price: '',
-      form_num : 1,
+      g_endtime:'',
+      g_price: {
+        g_price1:'',
+        g_price2:'',
+        g_price3:'',
+        g_price4:'',
+        g_price5:'',
+        g_price6:'',
+      },
     }
   },
   components: {
@@ -53,9 +113,24 @@ export default {
     goalStore() {
       console.log('********** front-end goalStore 호출 **********');
       var u_num = this.u_num;
-      var cate_num = this.cate_num;
+      // var cate_num = this.cate_num;
+      var cate_num = [];
+      cate_num.push(this.cate_num.cate_num1);
+      cate_num.push(this.cate_num.cate_num2);
+      cate_num.push(this.cate_num.cate_num3);
+      cate_num.push(this.cate_num.cate_num4);
+      cate_num.push(this.cate_num.cate_num5);
+      cate_num.push(this.cate_num.cate_num6);
+      var g_price = [];
+      g_price.push(this.g_price.g_price1);
+      g_price.push(this.g_price.g_price2);
+      g_price.push(this.g_price.g_price3);
+      g_price.push(this.g_price.g_price4);
+      g_price.push(this.g_price.g_price5);
+      g_price.push(this.g_price.g_price6);
       var g_time = this.g_time;
-      var g_price = this.g_price;
+      var g_endtime = this.g_endtime;
+      // var g_price = this.g_price;
         axios({
           method: 'post',
           url: 'api/goal/save_goal',
@@ -63,6 +138,7 @@ export default {
             u_num:this.$session.get('session'),
             cate_num: cate_num,
             g_time: g_time,
+            g_endtime: g_endtime,
             g_price: g_price
           }
         }).then(function(response) {
@@ -79,17 +155,17 @@ export default {
 </script>
 
 
-    <style>
-        .box-container{
-            border-style: solid;
-            border-width: 2px;
-            padding: 10px;
-        }
-        .table-header{
-            font-size: 20px;
-            font-weight: bold;
-        }
-        .table-color{
-            background-color: lightgrey;
-        }
-    </style>
+<style>
+    .box-container{
+        border-style: solid;
+        border-width: 2px;
+        padding: 10px;
+    }
+    .table-header{
+        font-size: 20px;
+        font-weight: bold;
+    }
+    .table-color{
+        background-color: lightgrey;
+    }
+</style>
