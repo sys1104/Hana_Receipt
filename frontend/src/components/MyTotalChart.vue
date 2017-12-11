dc1b3215b096806d70f655955a48ab14b1f61542
kwonsohyeon <thgus1023@hanmail.net>
2017-12-11 19:43:50 +0900

    첫번째 그래프 변경

--- frontend/src/components/MyTotalChart.vue
+++ frontend/src/components/MyTotalChart.vue
@@ -24,120 +24,123 @@ export default {
       u_name : '',
       mytotalconfig: {
         graphset: [{
-          type: "bar",
-          x: 55,
-          y: 40,
-          height: "100%",
-          width: "100%",
-          title: {
-            text: "나의 목표",
-            y: "80%",
-            fontColor: "#515151",
-            bold: true
-          },
-          scaleY: {
-            lineColor: "none",
-            values: "0:100:10",
-            guide: {
-              visible: true
-            },
-            tick: {
-              margin: 3,
-              lineWidth: 4,
-              size: 15,
-              lineColor: "#5F5F5F",
-              placement: "outter",
-              rules: [{
-                rule: "%i % 2 == 1",
-                lineWidth: 2
-              }]
-            },
-            minorTicks: 4,
-            minorTick: {
-              lineColor: "#C1C1C1",
-              placement: "inner",
-              size: 7
-            },
-            item: {
-              fontSize: 16,
-              rules: [{
-                rule: "%i % 2 == 1",
-                visible: false
-              }]
-            }
-          },
-          "scale-y-2": {
-            values: "0:100:10",
-            lineColor: "none",
-            item: {
-              visible: false
-            },
-            guide: {
-              visible: false
-            },
-            tick: {
-              placement: "inner",
-              size: 40,
-              padding: 0,
-              margin: 0,
-              offsetX: 50,
-              lineColor: "#fff",
-              rules: [{
-                rule: "%i == 0 || %i == 10",
-                visible: false
-              }]
-            }
-          },
-          plot: {
-            barsOverlap: "100%",
-            borderRadius: 2,
-            hoverState: {
-              visible: false
-            },
-            tooltip: {
-              visible: false
-            }
-          },
-          plotarea: {
-            marginBottom: "30%",
-            marginTop: "0%",
-            _marginRight: "0%"
-          },
-          scaleX: {
-            visible: false
-          },
-          series: [{
-              values: [100],
-              backgroundColor: "#fff",
-              borderWidth: 4,
-              borderColor: "#C1C1C1",
-              maxTrackers: 0,
-              barWidth: "100%",
-              tooltip: {
-                visble: false
-              }
-            },
-            {
-              values: [],
-              backgroundColor: "#F8B237",
-              barWidth: "93%",
-              maxTrackers: 0,
-              tooltip: {
-                visble: false
+          
+              type: "gauge",
+              globals: {
+                fontSize: 25
               },
-              valueBox: {
-                text: "%v",
-                placement: "bottom-out",
-                offsetY: 0,
-                fontSize: 28,
-                fontColor: "#515151",
+              plotarea:{
+                marginTop:60
+              },
+              plot:{
+                size:'100%',
+                valueBox: {
+                  placement: 'center',
+                  text:'%v', //default
+                  fontSize:30,
+                  rules:[
+                    {
+                      rule: '%v > 0 && %v <= 20',
+                      text: '%v<br><br>EXCELLENT'
+                    },
+                    {
+                      rule: '%v > 20 && %v <= 40',
+                      text: '%v<br><br>Good'
+                    },
+                    {
+                      rule: '%v > 40 && %v <= 60',
+                      text: '%v<br><br>FAIR'
+                    },
+                    {
+                      rule: '%v > 60 && %v <= 80',
+                      text: '%v<br><br>DANGER'
+                    },
+                    {
+                      rule: '%v > 80 && %v <= 100',
+                      text: '%v<br><br>BAD'
+                    },
+                    // {
+                    //   rule: '%v == 100',
+                    //   text: '%v<br><br>목표치 달성'
+                    // },
+                    // {
+                    //   rule: '%v >= 100',
+                    //   text: '%v<br><br>목표치 초과'
+                    // }
+                  ]
+                }
+              },
+              tooltip:{
+                borderRadius:5
+              },
+              scaleR:{
+                aperture:180,
+                minValue:0,
+                maxValue:100,
+                step:20,
+                center:{
+                  visible:false
+                },
+                tick:{
+                  visible:false
+                },
+                item:{
+                  offsetR:0,
+                  rules:[
+                    {
+                      // rule:'%i == 4',
+                      // offsetX:0
+                    }
+                  ]
+                },
+                // labels:['20','40','60','80','100'],
+                ring:{
+                  size:50,
+                  rules:[
+                    {
+                      rule:'%v > -20 && %v <= 0',
+                      backgroundColor:'black'
+                    } ,
+                    {
+                      rule:'%v > 0 && %v <= 20',
+                      backgroundColor:'#29B6F6'
+                    },
+                    {
+                      rule:'%v > 20 && %v <= 40',
+                      backgroundColor:'#FFA726'
+                    },
+                    {
+                      rule:'%v > 40 && %v <= 60',
+                      backgroundColor:'#EF5350'
+                    },
+                    {
+                      rule:'%v > 60 && %v <= 80',
+                      backgroundColor:'red'
+                    },
+                    // {
+                    //   rule:'%v > 80 && %v <= 100',
+                    //   backgroundColor:'red'
+                    // }  
+                  ]
+                }
+              },
+              series : [
+                {
+                  values : [], // starting value
+                  backgroundColor:'black',
+                  indicator:[],
+                  animation:{  
+                    effect:1,
+                    method:1,
+                    sequence:6,
+                    speed: 800
+                },
+                }
+              ]
+                }]
               }
             }
-          ]
-        }]
-      }
-    }
-  },
+          },
   created() {
     var self = this;
     var now = new Date();
