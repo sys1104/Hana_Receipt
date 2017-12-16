3a6f15c1fef2966f6ae32374401364f3895b15ab
kwonsohyeon <thgus1023@hanmail.net>
2017-12-16 16:52:48 +0900

    워드클라우드 문구

--- frontend/src/components/FourthSection.vue
+++ frontend/src/components/FourthSection.vue
@@ -2,7 +2,8 @@
 <div v-show="$session.exists() == true" id='hi'>
   <div class="container">
     <br>
-  <h2>워드클라우드</h2>
+    <h2>나의 소비 키워드</h2>
+  <h7>내가 자주 소비한 내역을 확인해보세요</h7>
       <div>
         <word-cloud></word-cloud>
       </div>
