5fa9230caf618b6fac31e692ddce33218faed9eb
강두영 <kangdy5491@naver.com>
2017-12-18 16:37:49 +0900

    컨슘히스토리 코드정리

--- backend/routes/consume_history.js
+++ backend/routes/consume_history.js
@@ -12,13 +12,12 @@ var requestHistory = function(req, res, callback) {
 
     if (database) {
         var axios = require('axios');
+        //savaHistory 메소드를 호출함으로써 DB에 값 저장 후 callback
         saveHistory(database, u_num, cate_num, content, price, c_time, wasted, function(err, result) {
             if (err) {
                 throw err;
             }
             if (result) {
-                // console.dir(result);
-                //일단 메인으로 redirect
                 res.redirect('http://localhost:8080');
             } else {
                 res.writeHead(200, {
@@ -35,7 +34,6 @@ var requestHistory = function(req, res, callback) {
         });
         res.write('<h1>데이터베이스 연결 실패</h1>');
         res.write('<div><p>DB에 연결 하지 못했습니다</p></div>');
-        res.write("<br/><a href='/public/adduser.html'>다시 가입하기</a>");
         res.end();
     }
 };
@@ -62,7 +60,7 @@ var saveHistory = function(database, u_num, cate_num, content, price, c_time, wa
             c_time: c_time,
             wasted: wasted
         };
-        //conn 객체를 사용해서 sql 실행
+        //conn 객체를 사용해서 sql 실행 (소비내역 저장 쿼리문)
         //set 모든 컬럼에 집어넣는 문법
         var exec = conn.query('insert into consume_history set ?', data, function(err, result) {
             //쿼리 작업 수행 후 반드시 연결을 해제 해야 한다.
@@ -88,7 +86,7 @@ var saveHistory = function(database, u_num, cate_num, content, price, c_time, wa
 
 // ==================== 소비내역 수정 function ==================== //
 var updateHistory = function(req, res, callback) {
-    console.log('********** updateHistory 호출됨 **********');
+    console.log('********** server-side updateHistory 호출됨 **********');
     var database = req.app.get('database');
     var paramConsumenum = Number(req.body.consume_num);
     var paramUnum = Number(req.body.u_num);
@@ -100,19 +98,18 @@ var updateHistory = function(req, res, callback) {
     //database --> true : DB에 접근할 수 있는 상태
     if (database) {
         var axios = require('axios');
+        //editHistory 메소드를 호출함으로써 DB에 값 수정 후 callback
         editHistory(database, paramConsumenum, paramUnum, paramCatenum, paramContent, paramPrice, paramCtime, paramWasted, function(err, result) {
             if (err) {
                 throw err;
             } // 에러 처리
             if (result) {
-                // console.dir(result);
                 res.redirect('http://localhost:8080');
             } else {
                 res.writeHead(200, {
                     "Content-Type": 'text/html;charset=utf8'
                 });
                 res.write('<h1>소비내역 업데이트 실패</h1>');
-                res.write("<br/><a href='/public/adduser.html'>다시 되돌아가기</a>");
                 res.end();
             }
         });
@@ -123,13 +120,12 @@ var updateHistory = function(req, res, callback) {
         });
         res.write('<h1>데이터베이스 연결 실패</h1>');
         res.write('<div><p>DB에 연결 하지 못했습니다</p></div>');
-        res.write("<br/><a href='/public/adduser.html'>다시 되돌아가기</a>");
         res.end();
     }
 };
 
 var editHistory = function(database, consume_num, u_num, cate_num, content, price, c_time, wasted, callback) {
-    console.log('********** editHistory 호출됨 **********');
+    console.log('********** server-side editHistory 호출됨 **********');
     //pool에서 커넥션 객체 가져오기
     database.pool.getConnection(function(err, conn) {
         if (err) {
@@ -141,8 +137,7 @@ var editHistory = function(database, consume_num, u_num, cate_num, content, pric
             return;
         }
         console.log('데이터베이스 연결 Thread' + conn.threadId);
-        // select consume_num from consume_history
-        //conn 객체를 사용해서 sql 실행
+        //conn 객체를 사용해서 sql 실행 (소비내역 수정 쿼리문)
         var exec = conn.query('update consume_history set cate_num = ?, content = ?, price = ?, c_time = ?, wasted =? where u_num = ? and consume_num = ?', [cate_num, content, price, c_time, wasted, u_num, consume_num],
             function(err, result) {
                 //쿼리 작업 수행 후 반드시 연결을 해제 해야 한다.
@@ -168,26 +163,25 @@ var editHistory = function(database, consume_num, u_num, cate_num, content, pric
 
 // ==================== 소비내역 삭제 function ==================== //
 var deleteHistory = function(req, res, callback) {
-    console.log('********** deleteHistory 호출됨 **********');
+    console.log('********** server-side deleteHistory 호출됨 **********');
     var database = req.app.get('database');
     var paramConsumenum = Number(req.body.consume_num);
     var paramUnum = Number(req.body.u_num);
 
     if (database) {
         var axios = require('axios');
+        //delHistory 메소드를 호출함으로써 DB 값 삭제 후 callback
         delHistory(database, paramConsumenum, paramUnum, function(err, result) {
             if (err) {
                 throw err;
             } // 에러 처리
             if (result) {
-                // console.dir(result);
                 res.redirect('http://localhost:8080');
             } else {
                 res.writeHead(200, {
                     "Content-Type": 'text/html;charset=utf8'
                 });
                 res.write('<h1>소비내역 삭제 실패</h1>');
-                res.write("<br/><a href='/public/adduser.html'>다시 되돌아가기</a>");
                 res.end();
             }
         });
@@ -198,13 +192,12 @@ var deleteHistory = function(req, res, callback) {
         });
         res.write('<h1>데이터베이스 연결 실패</h1>');
         res.write('<div><p>DB에 연결 하지 못했습니다</p></div>');
-        res.write("<br/><a href='/public/adduser.html'>다시 되돌아가기</a>");
         res.end();
     }
 };
 
 var delHistory = function(database, consume_num, u_num, callback) {
-    console.log('********** delHistory 호출됨 **********');
+    console.log('********** server-side delHistory 호출됨 **********');
     //pool에서 커넥션 객체 가져오기
     database.pool.getConnection(function(err, conn) {
         if (err) {
@@ -216,7 +209,7 @@ var delHistory = function(database, consume_num, u_num, callback) {
             return;
         }
         console.log('데이터베이스 연결 Thread' + conn.threadId);
-        //conn 객체를 사용해서 sql 실행
+        //conn 객체를 사용해서 sql 실행 (소비내역 삭제 쿼리문)
         var exec = conn.query('delete from consume_history where consume_num =?', consume_num,
             function(err, result) {
                 //쿼리 작업 수행 후 반드시 연결을 해제 해야 한다.
@@ -243,8 +236,7 @@ var delHistory = function(database, consume_num, u_num, callback) {
 
 // ==================== 소비내역 리스트 function ==================== //
 var read_consume_board = function(database, u_num, callback) {
-    console.log('********** read consume list 호출 **********');
-    // console.dir(database);
+    console.log('********** server-side read_consume_list 호출 **********');
     database.pool.getConnection(function(err, conn) {
         if (err) {
             if (conn) {
@@ -253,19 +245,17 @@ var read_consume_board = function(database, u_num, callback) {
             callback(err, null);
             return;
         }
-        // console.log('데이터 베이스 연결 스레드 아이디 : ' + conn.threadID);
-        //칼럼명을 배열로 만들기
-        // var sql = 'select * from consume_history order by c_time';
+        //conn 객체를 사용해서 sql 실행 (소비내역 조회 쿼리문)
         var sql = 'select consume_num, u_num, cate_num,content,price,substring(c_time,1,10) as c_time,wasted from consume_history where u_num = ? order by c_time desc';
-        // var columns = [post_num];
         var exec = conn.query(sql, [u_num], function(err, rows) {
-            //select의 결과물은 배열로 들어온다. -rows 변수..
+          //결과가 있으면 if문 성립
             if (rows.length > 0) {
                 conn.release();
                 callback(null, rows);
             } else {
+              //결과가 없으면 else문 성립
                 conn.release();
-                console.log('일치하는 소비내역 없음 --> 길이=0');
+                console.log('******* read_consume_board 메소드 일치하는 소비내역 없음 --> 길이=0 ********');
                 callback(null, null);
             }
         });
@@ -279,17 +269,13 @@ var read_consume_board = function(database, u_num, callback) {
 };
 
 var consumeList = function(req, res, callback) {
-    console.log('********** consumeList 호출 **********');
-    //아이디와 패스워드 받고 dB에 접근
-    //database --> true : DB에 접근 할 수 있는 상태
+    console.log('********** server-side consumeList 호출 **********');
     var database = req.app.get('database');
     var u_num = req.body.u_num;
-    // console.dir(database);
-    // var paramPostNum = req.query.tmp_post_num;
 
     if (database) {
+      //read_consume_board 메소드를 호출함으로써 DB 조회 후 소비내역 callback
         read_consume_board(database, u_num, function(err, rows) {
-
             if (err) {
                 console.error('read board 중 에러바생 ' + err.stack);
                 res.writeHead(200, {
@@ -300,23 +286,13 @@ var consumeList = function(req, res, callback) {
                 res.end();
             }
             if (rows) {
+              //소비내역이 있으면 실행
                 var data = {};
-                //데이터베이스 접속이 성공 했을 경우
-                // console.dir(rows);
-                // console.log(rows);
-                console.log('하이루 리스트보여줄까~?');
+                console.log('******* 소비내역 리스트 rows 있음 *******');
                 data = rows;
                 res.json(data);
-
             } else {
-                //데이터베이스 접속이 실패 했을 경우
-                // res.writeHead(200, {
-                //   'Content-Type': 'text/html;charset=utf8'
-                // });
-                // res.write('<h1>read board 실패</h1>');
-                // res.write('<div><p>확인하세요.</p></div>');
-                // res.end();
-                console.log('로우가없음');
+                console.log('******* 소비내역 리스트 rows 없음 *******');
                 var data2 = {};
                 res.json(data2);
             }
@@ -328,21 +304,18 @@ var consumeList = function(req, res, callback) {
         });
         res.write('<h1>데이터베이스 연결 실패</h1>');
         res.write('<div><p>DB에 연결 하지 못했습니다.</p></div>');
-        res.write('<br/> <a href="/public/login2.html">다시 로그인 </a>');
         res.end();
     }
 };
 
-
-// ========================= server-side 1주일간 낭비되는 금액 카테고리별 출력 function =================== //
+// ========================= server-side 지난 목표기간 동안 낭비된 금액 카테고리별 출력 function =================== //
 var wastedList = function(req, res, callback) {
-    console.log('********** server-side 목표대비 사용금액 분석 function 호출 **********');
+    console.log('********** server-side wastedList 호출 **********');
     var database = req.app.get('database');
-    var u_num = req.body.u_num; //vue에서 받아와야 함!!!
-    var start_date = Number(req.body.start_date); //front단에서 오늘 기준으로 주일의 시작
-    console.log('start_date 값 : ' + start_date);
+    var u_num = req.body.u_num;
+    var start_date = Number(req.body.start_date); //현재 날짜 받아오기.
     if (database) {
-
+      //wasted_used 메소드를 호출함으로써 DB 조회 후 낭비된 소비내역 callback
         wasted_used(database, u_num, start_date, function(err, rows) {
             if (err) {
                 console.error('********** cate_used 에러 발생 **********' + err.stack);
@@ -355,14 +328,14 @@ var wastedList = function(req, res, callback) {
                 // 에러 처리
             }
             if (rows) {
+              //낭비된 내역이 있으면 실행
                 var data = {};
                 data = rows;
                 res.json(data);
-                console.log('********** data json 형태로 Wasted.vue로 보냄 **********')
-                console.log(data);
+                console.log('********** wasted_used 낭비된 내역 있음 (Wasted.vue로 response) **********')
                 res.end();
             } else {
-                console.log("********** 사용내역 없음 **********");
+                console.log("********** wasted_used 낭비된 내역 없음 **********");
             }
         });
     } else {
@@ -372,15 +345,13 @@ var wastedList = function(req, res, callback) {
         });
         res.write('<h1>데이터베이스 연결 실패</h1>');
         res.write('<div><p>DB에 연결 하지 못했습니다</p></div>');
-        res.write("<br/><a href='/public/login2.html'>다시 로그인</a>");
         res.end();
     }
 };
 
-// ========================= server-side 카테고리별 사용금액 분석 function =================== //
 var wasted_used = function(database, u_num, start_date, callback) {
     console.log('********** server-side wasted_used 호출 **********');
-    database.pool.getConnection(function(err, conn) { //DB 접속
+    database.pool.getConnection(function(err, conn) {
         if (err) {
             if (conn) {
                 conn.release();
@@ -388,18 +359,16 @@ var wasted_used = function(database, u_num, start_date, callback) {
             callback(err, null);
             return;
         }
-        console.log('데이터베이스 연결 스레드 아이디 : ' + conn.threadId);
+        //conn 객체를 사용해서 sql 실행 (지난 목표기간 동안 낭비내역 조회 쿼리문)
         var exec = conn.query('select cate_num, sum(price) sum_price from consume_history where u_num = ? and wasted = 1 and c_time >= (select max(g_time) from goal where u_num = ? and g_endtime <= ?) and c_time <= ? group by cate_num', [u_num, u_num, start_date, start_date],
             function(err, rows) {
-                //select의 결과물은 배열로 들어온다. rows 변수...
                 if (rows.length > 0) {
                     conn.release();
                     console.log('********** 카테고리별 낭비금액 rows로 반환 **********');
-                    console.log(rows);
                     callback(null, rows);
                 } else {
                     conn.release();
-                    console.log('********** 사용내역이 없습니다.**********');
+                    console.log('********** 카테고리별 낭비금액 rows로 반환하지 못함 **********');
                     callback(null, null);
                 }
             });
@@ -412,7 +381,7 @@ var wasted_used = function(database, u_num, start_date, callback) {
     });
 };
 
-
+//모듈 exports
 module.exports.requestHistory = requestHistory;
 module.exports.updateHistory = updateHistory;
 module.exports.consumeList = consumeList;
