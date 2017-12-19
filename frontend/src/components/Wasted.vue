9a1f7da3e2ca31528d65fffe19a592986ad27fa5
JeonHanShin <hanshin234@naver.com>
2017-12-19 09:59:46 +0900

    낭비 기간 수정

--- frontend/src/components/Wasted.vue
+++ frontend/src/components/Wasted.vue
@@ -85,7 +85,9 @@ export default {
         self.sum = sum2;
         var period = {};
         period = response.data.period;
-        self.period = '목표 기간 ' + response.data.period[0].max_gtime + ' ~ ' + response.data.period[0].max_gendtime + ' 동안 낭비된 금액';
+        if(response.data.period[0].max_gtime != null){
+          self.period = '목표 기간 ' + response.data.period[0].max_gtime + ' ~ ' + response.data.period[0].max_gendtime + ' 동안 낭비된 금액';
+        }
       } catch (e) {
         console.log('********** front-end 목표 내역이 없습니다. **********');
       }
